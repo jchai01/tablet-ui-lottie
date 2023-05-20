1b2f4f8a106d3e1af43c617557f82db42e76cf19
Joseph <josephchai100@hotmail.com>
2023-05-20 17:56:18 +0800

    update functionality

--- js/feedback.js
+++ js/feedback.js
@@ -4,6 +4,12 @@ setTimeout(
     }, 5000
 );
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// get param value for location
+const l = urlParams.get('l')
+
 let veryPoor = document.querySelector('#very-poor');
 let poor = document.querySelector('#poor');
 let neutral = document.querySelector('#neutral');
@@ -51,41 +57,81 @@ lottie.loadAnimation({
 });
 
 veryPoor.addEventListener('click', async () => {
+    data = {} // empty data required to trigger the server to read query string.
+
     window.location.href = "thankyou.html";
-    await fetch('http://localhost:1234/test')
+    
+    // send location as l, rating as r
+    await fetch('http://localhost:1234/submit?l=' + l + '&r=1', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
     .then((response) => {
       console.log(response)
     });
 });
 
 poor.addEventListener('click', async () => {
+    data = {}
     window.location.href = "thankyou.html";
-    await fetch('http://localhost:1234/test')
+    await fetch('http://localhost:1234/submit?l=' + l + '&r=2', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
     .then((response) => {
       console.log(response)
     });
 });
 
+
 neutral.addEventListener('click', async () => {
+    data = {}
     window.location.href = "thankyou.html";
-    await fetch('http://localhost:1234/test')
+    await fetch('http://localhost:1234/submit?l=' + l + '&r=3', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
     .then((response) => {
       console.log(response)
     });
 });
 
 good.addEventListener('click', async () => {
+    data = {}
     window.location.href = "thankyou.html";
-    await fetch('http://localhost:1234/test')
+    await fetch('http://localhost:1234/submit?l=' + l + '&r=4', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
     .then((response) => {
       console.log(response)
     });
 });
 
 excellent.addEventListener('click', async () => {
+    data = {}
     window.location.href = "thankyou.html";
-    await fetch('http://localhost:1234/test')
+    await fetch('http://localhost:1234/submit?l=' + l + '&r=5', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
     .then((response) => {
       console.log(response)
     });
 });
+
--- js/home.js
+++ js/home.js
@@ -5,21 +5,21 @@ let lounge = document.querySelector('#lounge');
 let toilets = document.querySelector('#toilets');
 
 workstations.addEventListener('click', function() {
-  window.location.href = "feedback.html";
+  window.location.href = "feedback.html?l=1";
 });
 
 pantry.addEventListener('click', function() {
-  window.location.href = "feedback.html";
+  window.location.href = "feedback.html?l=2";
 });
 
 meeting.addEventListener('click', function() {
-  window.location.href = "feedback.html";
+  window.location.href = "feedback.html?l=3";
 });
 
 lounge.addEventListener('click', function() {
-  window.location.href = "feedback.html";
+  window.location.href = "feedback.html?l=4";
 });
 
 toilets.addEventListener('click', function() {
-  window.location.href = "feedback.html";
+  window.location.href = "feedback.html?l=5";
 });
